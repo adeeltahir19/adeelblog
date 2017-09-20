4b32528fdee3ca94126cf6bc346370ec01d1b162
Adeel Tahir <adeeltahir19@gmail.com>
2017-09-20 10:31:35 +0500

    crud implement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,46 @@
 class PostsController < ApplicationController
 
 	def index
-		@content = 'Here comes a paragraph :) ' ;
+		@posts = Post.all
 	end
 	def new
-
+		@post = Post.new
+		@category = Category.all
 	end
 	def create
+		@post = Post.create(allowed_params)
+		if @post.save
+			flash[:notice] = "post is saved successfully"
+			redirect_to posts_path
+			
+		else
+			render "new"
+		end
 
 	end
 	def edit
-
+		@post = Post.find(params[:id])
 	end
 	def update
-
+		@post = Post.find(params[:id])
+		if @post.update_attributes(allowed_params)
+			flash[:notice] = "Post is updated"
+			redirect_to post_path
+		else
+			render "edit"
+		end
 	end
 	def show
-
+		@post = Post.find(params[:id])
 	end
 	def destroy
-
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+		flash[:notice] = "Post is deleted"
 	end
+
+	def allowed_params
+  		params.require(:post).permit(:title, :body, :category_id )
+	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   
+  get 'categories/index'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
+  get 'categories/show'
+
 	get 'home/index'
 
 	root 'home#index'
 	
 	resources :posts
+	resources :categories
 
   
 end
